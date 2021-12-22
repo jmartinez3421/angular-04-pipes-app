72bc9acc3cd8792dfd66c85673e140ae72cf9b8e
Jordi Martinez <jmartinezg.fp@gmail.com>
2021-12-22 13:58:59 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
